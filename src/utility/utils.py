70133b1644b2c429ba6cdf646f4b37a020918491
Rendy <maxgreg13625@gmail.com>
2024-07-27 16:19:56 +0800

    add export_config and update main

--- src/utility/utils.py
+++ src/utility/utils.py
@@ -13,3 +13,13 @@ def get_config(path: str='') -> ConfigParser:
         print(f"config file: {path} doesn't exist..")
         config['Required'] = {'test': 'test'}
     return config
+
+def export_config(config: ConfigParser, path: str=''):
+    if path == '':
+        path = os.path.join(os.getcwd(), 'setting', 'encrypt_config.ini')
+
+    # in order to keep config key in original case
+    config.optionxform = lambda option: option
+
+    with open(path, 'w') as configfile:
+        config.write(configfile)
