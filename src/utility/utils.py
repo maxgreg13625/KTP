c547661ed49d7d015b4e10f75055dd3b9d4abe77
Rendy <maxgreg13625@gmail.com>
2024-07-27 15:45:09 +0800

    update get_config and pytest version

--- src/utility/utils.py
+++ src/utility/utils.py
@@ -5,6 +5,11 @@ def get_config(path: str='') -> ConfigParser:
     config = ConfigParser()
     if path == '':
         path = os.path.join(os.getcwd(), 'setting', 'config.ini')
- 
-    config.read(path)
+
+    try:
+        with open(path) as f:
+            config.read_file(f)
+    except:
+        print(f"config file: {path} doesn't exist..")
+        config['Required'] = {'test': 'test'}
     return config
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,4 +2,7 @@ from src.utility import *
 
 def test_get_config():
     config = get_config()
-    assert 'Required' in config.sections() 
+    assert 'Required' in config.sections()
+
+    config = get_config('test.int')
+    assert 'Required' in config.sections()
